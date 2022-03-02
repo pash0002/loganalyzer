1ec1836a22a6dfd206f47f4f90ee6b2d6abbb0dc
prasad.kudale <prasad.kudale@datametica.com>
2022-03-02 16:00:06 +0530

    refactoring and added another API

--- src/main/java/com/example/loganalyzer/controller/parser/LogParserController.java
+++ src/main/java/com/example/loganalyzer/controller/parser/LogParserController.java
@@ -14,14 +14,14 @@ public class LogParserController {
 
   List<LogData> listOfLogData;
 
-  @GetMapping("/parse")
+  @GetMapping("api/parse")
   public ResponseEntity<List<LogData>> getParsedLogs(@RequestParam(name="path") String filePath) {
     LogParser parser = new LogParser(filePath);
     this.listOfLogData = parser.parse();
     return new ResponseEntity<>(this.listOfLogData, HttpStatus.OK);
   }
 
-  @GetMapping("/parse/level")
+  @GetMapping("api/parse/level")
   public ResponseEntity<List<LogData>> getParsedLogsByLevel(@RequestParam(name="l") String level) {
     return new ResponseEntity<>(new LogLevelService(listOfLogData).getLogsDataByLevel(level), HttpStatus.OK);
   }
